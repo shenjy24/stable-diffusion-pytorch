8a24dacd90ed7a01143f5d4e6d6fff80adc9dc1a
shenjy24 <shenjy24@gmail.com>
2023-08-27 10:33:14 +0800

    代码优化

--- main.py
+++ main.py
@@ -1,32 +1,13 @@
-# This is a sample Python script.
-
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
 import time
+
 import torch
+from diffusers import StableDiffusionPipeline, EulerDiscreteScheduler, DiffusionPipeline
 from huggingface_hub import snapshot_download
-from diffusers import StableDiffusionPipeline, DPMSolverMultistepScheduler, EulerDiscreteScheduler
 
 from utils.utils import generate_random_str
 
 
-def local_model(prompt):
-    """
-    加载本地模型
-    :param prompt: 提示词
-    """
-    start_time = time.time()
-
-    # 直接从缓存目录.cache中获取，将随机数目录名改为majicmix
-    pipe = StableDiffusionPipeline.from_pretrained("./model/majicmix", torch_dtype=torch.float16)
-    pipe = pipe.to("cuda")
-    image = pipe(prompt).images[0]
-    image.save(f"image/{generate_random_str() + local_model.__name__}.png")
-
-    print(f"函数 {local_model.__name__} 的运行时间为: {time.time() - start_time} 秒")
-
-
-def local_model_2(positive_prompt, negative_prompt):
+def local_model(positive_prompt, negative_prompt):
     """
     加载本地模型优化版本
     :param positive_prompt: 正面提示词
@@ -34,27 +15,30 @@ def local_model_2(positive_prompt, negative_prompt):
     """
     start_time = time.time()
 
-    pipe = StableDiffusionPipeline.from_pretrained("./model/majicmix", torch_dtype=torch.float16, safety_checker=None)
+    # 直接从缓存目录.cache中获取，将随机数目录名改为majicmix
+    pipe = StableDiffusionPipeline.from_pretrained("./model/majicmix", torch_dtype=torch.float16,
+                                                   safety_checker=None)
     # pipe.scheduler = DPMSolverMultistepScheduler.from_config(pipe.scheduler.config)
     pipe.scheduler = EulerDiscreteScheduler.from_config(pipe.scheduler.config)
     pipe = pipe.to("cuda")
     generator = torch.Generator("cuda").manual_seed(0)
     image = pipe(prompt=positive_prompt, negative_prompt=negative_prompt, generator=generator,
                  num_inference_steps=20).images[0]
-    image.save(f"image/main/{generate_random_str() + local_model_2.__name__}.png")
+    image.save(f"image/main/{generate_random_str() + local_model.__name__}.png")
 
-    print(f"函数 {local_model_2.__name__} 的运行时间为: {time.time() - start_time} 秒")
+    print(f"函数 {local_model.__name__} 的运行时间为: {time.time() - start_time} 秒")
 
 
-def from_pretrained(prompt):
+def remote_model(positive_prompt, negative_prompt):
     """
     从 hugging face 获取预训练模型
-    :param prompt: 提示词
+    :param negative_prompt: 正面提示词
+    :param positive_prompt: 负面提示词
     """
-    pipe = StableDiffusionPipeline.from_pretrained("CompVis/stable-diffusion-v1-4", torch_dtype=torch.float16)
+    pipe = DiffusionPipeline.from_pretrained("SG161222/Realistic_Vision_V2.0", torch_dtype=torch.float16)
     pipe = pipe.to("cuda")
-    image = pipe(prompt).images[0]
-    image.save(f"image/{generate_random_str()}.png")
+    image = pipe(prompt=positive_prompt, negative_prompt=negative_prompt, num_inference_steps=20).images[0]
+    image.save(f"image/main/{generate_random_str() + '_' + remote_model.__name__}.png")
 
 
 def download_model(repo):
@@ -70,8 +54,6 @@ if __name__ == '__main__':
     print(torch.cuda.is_available())
     # p = "beautiful dog"
     pp = "Best quality, masterpiece, ultra high res, (photorealistic:1.4), 1girl"
-    np = "ng_deepnegative_v1_75t, badhandv4"
-    # local_model(p)
-    local_model_2(pp, np)
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    np = "badhandv4"
+    local_model(pp, np)
+    # remote_model(pp, np)
